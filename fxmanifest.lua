9ac343a198ccf0cc30696fa26553ae01da12dc35
Niclas | Ravish <43492760+NequZ@users.noreply.github.com>
2025-11-23 17:20:20 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'rdr3' -- oder 'gta5' je nachdem
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+name 'pe_blackjack'
+description 'Blackjack - Pioneers Echoes'
+author 'Niclas'
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+    'html/ui.js',
+    'html/ui.css'
+}
